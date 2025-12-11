956f97ea5d91ca1c65c52456867f51a6e5022e21
onechapter <onechapter93@gmail.com>
2025-12-11 19:55:40 +0700

    chore: use vibration plugin and unblock kotlin build

--- vietmap_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ vietmap_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -15,7 +15,7 @@ import shared_preferences_foundation
 import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
+  ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
